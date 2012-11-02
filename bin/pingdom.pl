fc8b035f790723ef2a9ac94f0df41a1c247b4022
Dominik Schulz <tex@cpan.org>
2012-11-02 17:00:57 +0000

    Import of TEX/Pingdom-Client-0.11 from CPAN.
    gitpan-cpan-distribution: Pingdom-Client
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         TEX/Pingdom-Client-0.11.tar.gz
    gitpan-cpan-author:       TEX
    gitpan-cpan-maturity:     released

---
+++ bin/pingdom.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Getopt::Long;
+use Pingdom::Client;
+
+my $username = undef;
+my $password = undef;
+my $apikey = undef;
+
+GetOptions(
+	'username=s'	=> \$username,
+	'password=s'	=> \$password,
+	'apikey=s'	=> \$apikey,
+);
+
+my $API = Pingdom::Client::->new({
+	'username' => $username,
+	'password' => $password,
+	'apikey'   => $apikey,
+});
+
+print Dumper($API->contacts());
+
+=head1 NAME
+
+pingdom cli
+
+=head1 VERSION
+
+version 0.11
+
+=cut
